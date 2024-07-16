8be68f6039fadb67d10afce647a12e2c3070e3b6
admin <afallaxva@gmail.com>
2024-07-17 02:28:23 +0300

    v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,25 @@ jQuery(document).ready(function($) {
     
     $('.level-bar-inner').css('width', '0');
 
+    function resizeCanvasToDisplaySize(canvas) {
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+
+        if (canvas.width !== width || canvas.height !== height) {
+            canvas.width = width;
+            canvas.height = height;
+            return true;
+        }
+
+        return false;
+    }
     
     $(window).on('load', function () {
 
-        function resizeCanvasToDisplaySize(canvas) {
-            const width = canvas.clientWidth;
-            const height = canvas.clientHeight;
+        let canvas = document.getElementById('myCanvas');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-            if (canvas.width !== width || canvas.height !== height) {
-                canvas.width = width;
-                canvas.height = height;
-                return true;
-            }
-
-            return false;
-        }
 
         $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
 
