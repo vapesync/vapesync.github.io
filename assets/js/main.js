f94cd17b51a2811027b2a68eb57c442a8aaea9cf
admin <afallaxva@gmail.com>
2024-07-17 01:27:10 +0300

    v+

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,14 @@ jQuery(document).ready(function($) {
     
     $('.level-bar-inner').css('width', '0');
     
-    $(window).on('load', function() {
+    $(window).on('load', function () {
+
+        $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+
+        $("#menu-icon").on("click", function () {
+            $("nav").slideToggle();
+            $(this).toggleClass("active");
+        });
 
         $('.level-bar-inner').each(function() {
         
