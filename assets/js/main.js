f04b4f9a5e596e284cc0fb497f17aa2a36c24a0f
admin <afallaxva@gmail.com>
2024-07-17 02:34:43 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,9 @@ jQuery(document).ready(function($) {
     
     
     $('.level-bar-inner').css('width', '0');
-
-    function resizeCanvasToDisplaySize(canvas) {
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-
-        if (canvas.width !== width || canvas.height !== height) {
-            canvas.width = width;
-            canvas.height = height;
-            return true;
-        }
-
-        return false;
-    }
     
     $(window).on('load', function () {
 
-        let canvas = document.getElementById('myCanvas');
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
 
 
         $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
